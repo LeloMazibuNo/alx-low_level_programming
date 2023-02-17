93de1a22098749f60a2e883904934d5665781a6d
LeloMazibuNo <n.mazibuko2@gmail.com>
2023-02-17 11:18:28 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line
- * Return: 0
+ * main - my first c programme using puts to print a line
+ * return : 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle"");
-return (0);
+retun (0);
 }
